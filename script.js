3e90fb27f12ce4f23f7a0c5277b1bf9153620fa1
rajisanjana3011 <rajisanjana3011@gmail.com>
2026-03-11 19:18:32 +0530

    assignment7

---
+++ script.js
@@ -0,0 +1,27 @@
+let display = document.getElementById("display");
+function appendValue(value){
+    display.value += value;
+}
+function clearDisplay(){
+    display.value = "";
+}
+function deleteLast(){
+    display.value = 
+    display.value.slice(0,-1);
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+function square(){
+    let num = parseFloat(display.value);
+    display.value = num * num;
+}
+function cube(){
+   let num = parseFloat(display.value);
+    display.value = num * num * num; 
+}
